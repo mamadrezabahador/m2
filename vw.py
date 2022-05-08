9659d0358e1553bd96c0115452bc85a553a1d7db
mamadrezabahador <100021985+mamadrezabahador@users.noreply.github.com>
2022-05-08 13:51:17 -0700

    Create vw.py

---
+++ vw.py
@@ -0,0 +1,3 @@
+What happens if you attempt to access an element of a list using a negative index?
+when we use nagative indexes
+to access an element , The list starts counting with the last element
